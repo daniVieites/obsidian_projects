8e624a5c45691ffbe693de517a721d374362ff61
a19danielvt <a19danielvt@iessanclemente.net>
2021-05-26 17:08:56 +0200

    insert y findall

---
+++ prueba/src/test/java/com/example/prueba/handler/NodeHandlerTest.java
@@ -0,0 +1,130 @@
+package com.example.prueba.handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.example.prueba.model.NodeDesc;
+import com.example.prueba.model.NodeRoot;
+import com.example.prueba.service.NodeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.List;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureWebTestClient
+@SpringBootTest
+class NodeHandlerTest {
+
+  @Autowired private WebTestClient client;
+  @MockBean private NodeService service;
+  private ObjectMapper mapper;
+
+  @BeforeEach
+  void setUp() {
+    mapper = new ObjectMapper();
+  }
+
+  @Test
+  void findAll() {
+    List<NodeRoot> nodes =
+        Arrays.asList(new NodeRoot("root"), new NodeDesc("desc", "node desc", null));
+    when(service.findAll()).thenReturn(Flux.fromIterable(nodes));
+
+    client
+        .get()
+        .uri("/nodes")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_NDJSON)
+        .expectBodyList(NodeRoot.class)
+        .consumeWith(
+            response -> {
+              List<NodeRoot> nodeList = response.getResponseBody();
+              assertNotNull(nodeList);
+              assertEquals(NodeRoot.class, nodeList.get(0).getClass());
+              assertEquals(NodeDesc.class, nodeList.get(1).getClass());
+              assertEquals("root", nodeList.get(0).getNombre());
+              assertEquals("desc", nodeList.get(1).getNombre());
+            })
+        .hasSize(2);
+
+    verify(service).findAll();
+  }
+
+  @Test
+  void insert() {
+    var nodeRoot = new NodeRoot();
+    nodeRoot.setNombre("root");
+
+    var parentId = new ObjectId();
+    var nodeDesc = new NodeDesc();
+    nodeDesc.setNombre("desc");
+    nodeDesc.setDescripcion("node desc");
+    nodeDesc.setParentId(parentId);
+
+    when(service.insert(any(NodeRoot.class)))
+        .then(
+            invocation -> {
+              NodeRoot nr = invocation.getArgument(0);
+              nr.setId(new ObjectId());
+              return Mono.just(nr);
+            });
+
+    client
+        .post()
+        .uri("/insert")
+        .contentType(MediaType.APPLICATION_NDJSON)
+        .bodyValue(nodeRoot)
+        .exchange()
+        .expectStatus()
+        .isCreated()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_NDJSON)
+        .expectBody(NodeRoot.class)
+        .consumeWith(
+            response -> {
+              NodeRoot n = response.getResponseBody();
+              assertNotNull(n);
+              assertNotNull(n.getId());
+              assertEquals("root", n.getNombre());
+            });
+
+    client
+        .post()
+        .uri("/insert")
+        .contentType(MediaType.APPLICATION_NDJSON)
+        .bodyValue(nodeDesc)
+        .exchange()
+        .expectStatus()
+        .isCreated()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_NDJSON)
+        .expectBody(NodeDesc.class)
+        .consumeWith(
+            response -> {
+              NodeDesc n = response.getResponseBody();
+              assertNotNull(n);
+              assertNotNull(n.getId());
+              assertEquals("desc", n.getNombre());
+              assertEquals("node desc", n.getDescripcion());
+              assertEquals(parentId.toString(), n.getParentId().toString());
+            });
+
+    verify(service, times(2)).insert(any(NodeRoot.class));
+  }
+}
--- prueba/src/test/java/com/example/prueba/service/NodeServiceTest.java
+++ prueba/src/test/java/com/example/prueba/service/NodeServiceTest.java
@@ -6,17 +6,14 @@ import static org.mockito.Mockito.*;
 import com.example.prueba.model.NodeDesc;
 import com.example.prueba.model.NodeRoot;
 import com.example.prueba.repository.NodeRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import java.util.List;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.engine.support.hierarchical.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
@@ -38,22 +35,25 @@ class NodeServiceTest {
   }
 
   @Test
-  void testFindAll() {
-    List<NodeRoot> nodes = Arrays.asList(new NodeRoot("node1"), new NodeRoot("node2"));
+  void findAll() {
+    List<NodeRoot> nodes =
+        Arrays.asList(new NodeRoot("root"), new NodeDesc("desc", "node desc", null));
     when(repository.findAll()).thenReturn(Flux.fromIterable(nodes));
 
     var response = service.findAll().collectList().block();
 
     assertNotNull(response);
     assertEquals(2, response.size());
-    assertEquals("node1", response.get(0).getNombre());
-    assertEquals("node2", response.get(1).getNombre());
+    assertEquals(NodeRoot.class, response.get(0).getClass());
+    assertEquals(NodeDesc.class, response.get(1).getClass());
+    assertEquals("root", response.get(0).getNombre());
+    assertEquals("desc", response.get(1).getNombre());
 
     verify(repository).findAll();
   }
 
   @Test
-  void testInsert() throws JsonProcessingException {
+  void insert() throws JsonProcessingException {
     var nodeRoot = new NodeRoot();
     nodeRoot.setNombre("root");
 
@@ -63,11 +63,13 @@ class NodeServiceTest {
     nodeDesc.setDescripcion("node desc");
     nodeDesc.setParentId(parentId);
 
-    when(repository.insert(any(NodeRoot.class))).then(invocation -> {
-      NodeRoot n = invocation.getArgument(0);
-      n.setId(new ObjectId());
-      return Mono.just(n);
-    });
+    when(repository.insert(any(NodeRoot.class)))
+        .then(
+            invocation -> {
+              NodeRoot n = invocation.getArgument(0);
+              n.setId(new ObjectId());
+              return Mono.just(n);
+            });
 
     // insert NodeRoot test
     var nodeRootInsert = service.insert(nodeRoot).block();
